76901de198e9343a624856efc9f509ea9fc1f129
khushal <khushal2004a@gmail.com>
2024-12-07 12:24:57 +0530

    added white bg

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
   return (
-    <div className='h-screen w-full flex justify-center items-center'>
+    <div className='h-screen w-full flex justify-center items-center bg-white'>
       <SignIn />
     </div>
   )
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -93,7 +93,7 @@ import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
   return (
-    <div className='h-screen w-full flex justify-center items-center'>
+    <div className='h-screen w-full flex justify-center items-center bg-white'>
       <SignUp />
     </div>
   )
